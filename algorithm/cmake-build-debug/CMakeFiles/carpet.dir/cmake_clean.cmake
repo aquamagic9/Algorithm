e915cf02b0427aa1903e6554734843dc4639fc12
aquamagic9 <84334547+aquamagic9@users.noreply.github.com>
2023-04-23 19:05:30 +0900

    carpet

---
+++ algorithm/cmake-build-debug/CMakeFiles/carpet.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/carpet.dir/Programmers/carpet.cpp.o"
+  "CMakeFiles/carpet.dir/Programmers/carpet.cpp.o.d"
+  "carpet"
+  "carpet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/carpet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

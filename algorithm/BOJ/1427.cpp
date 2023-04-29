716efa4b2ba3b95ab31f6e67694bd8470bdb9e51
hyuncho <84334547+aquamagic9@users.noreply.github.com>
2023-04-29 23:16:31 +0900

    1427

--- algorithm/BOJ/1427.cpp
+++ algorithm/BOJ/1427.cpp
@@ -3,6 +3,22 @@ using namespace std;
 
 int main()
 {
-    cout << "hi";
+    string num;
+
+    cin >> num;
+    for (int i = 0; i < num.size() - 1; i++){
+        char max = i;
+        for (int j = i + 1; j < num.size(); j++){
+            if (num[max] < num[j]){
+                max = j;
+            }
+        }
+        int tmp = num[max];
+        num[max] = num[i];
+        num[i] = tmp;
+    }
+
+    cout << num;
+
     return 0;
 }

8b1b0d6da192dea4538b451ea4cb937948ee6feb
aquamagic9 <84334547+aquamagic9@users.noreply.github.com>
2023-05-02 13:05:13 +0900

    10989

--- algorithm/BOJ/10989.cpp
+++ algorithm/BOJ/10989.cpp
@@ -8,13 +8,13 @@ int main()
     int count[10001] = {0};
     int number = 0;
 
-    for (int i = 0; i <= N; i++){
+    for (int i = 1; i <= N; i++){
         cin >> number;
         count[number]++;
     }
     for (int i = 0; i <= 10000; i++){
         if (count[i] != 0){
-            for(int j = 0; j < count[j]; j++)
+            for(int j = 0; j < count[i]; j++)
                 cout << i << "\n";
         }
     }

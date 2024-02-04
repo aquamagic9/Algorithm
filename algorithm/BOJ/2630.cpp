6226710bb28b7973ee9fe7652d84208eb4c5eb78
hyuncho <84334547+aquamagic9@users.noreply.github.com>
2024-02-05 00:04:42 +0900

    2630

--- algorithm/BOJ/2630.cpp
+++ algorithm/BOJ/2630.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 void CutArray(vector<vector<int>> &arr, int ax, int ay, int bx, int by);
+bool CheckArr(vector<vector<int>> &arr, int ax, int ay, int bx, int by);
 int blueCnt = 0;
 int whiteCnt = 0;
 
@@ -27,7 +28,7 @@ int main()
 
 void CutArray(vector<vector<int>> &arr, int ax, int ay, int bx, int by)
 {
-    if (ax == bx && ay == by)
+    if (ax == bx && ay == by || CheckArr(arr, ax, ay, bx, by))
     {
         if (arr[ay][ax] == 1)
             blueCnt++;
@@ -35,9 +36,25 @@ void CutArray(vector<vector<int>> &arr, int ax, int ay, int bx, int by)
             whiteCnt++;
         return;
     }
-    int length = bx - ax;
-    CutArray(arr, 0, 0, length / 2, length / 2);
-    CutArray(arr, length / 2 + 1, 0, bx, length / 2);
-    CutArray(arr, 0, length / 2 + 1, length / 2, by);
-    CutArray(arr, length / 2, length / 2, bx, by);
+    int length = (bx - ax) / 2;
+    CutArray(arr, ax, ay, ax + length, ay + length);
+    CutArray(arr, ax + length + 1, ay, bx, ay + length);
+    CutArray(arr, ax, ay + length + 1, ax + length, by);
+    CutArray(arr, ax + length + 1, ay + length + 1, bx, by);
+}
+
+bool CheckArr(vector<vector<int>> &arr, int ax, int ay, int bx, int by)
+{
+    int color = arr[ay][ax];
+    for (int i = ay; i <= by; i++)
+    {
+        for (int j = ax; j <= bx; j++)
+        {
+            if (color != arr[i][j])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
 }

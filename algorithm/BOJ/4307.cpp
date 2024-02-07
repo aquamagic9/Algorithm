3cae8d864742ecfc48c30c96fc551c1465a973ad
aquamagic9 <84334547+aquamagic9@users.noreply.github.com>
2024-02-07 18:55:43 +0900

    4307 (resolve)

--- algorithm/BOJ/4307.cpp
+++ algorithm/BOJ/4307.cpp
@@ -12,28 +12,14 @@ int main()
     {
         int l, n;
         cin >> l >> n;
-        vector<int> posVec;
-        int minDiff = l / 2;
-        int tmp;
-        for (int j = 0; j < n; j++) {
-            cin >> tmp;
-            if (tmp == 0 || tmp == l)
-                continue;
-            posVec.push_back(tmp);
-            if (minDiff > abs(tmp - l / 2))
-                minDiff = abs(tmp - l / 2);
+
+        int minTime = 0, maxTime = 0, pos;
+        for (int i = 0; i < n; i++){
+            cin >> pos;
+            minTime = max(minTime, min(pos, l - pos));
+            maxTime = max(maxTime, max(pos, l - pos));
         }
-        int max;
-        if (!posVec.empty())
-        {
-            sort(posVec.begin(), posVec.end());
-            max = posVec.front() < l - posVec.back() ? l - posVec.front() : posVec.back();
-        }
-        else
-        {
-            max = 0;
-        }
-        cout << l / 2 - minDiff << " " << max << '\n';
+        cout << minTime << " " << maxTime << '\n';
     }
 
     return 0;

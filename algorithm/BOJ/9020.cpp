d49907dabd1f012d9b9e21c09cdb888ae0d2a022
hyuncho <84334547+aquamagic9@users.noreply.github.com>
2024-02-25 01:59:41 +0900

    9020

---
+++ algorithm/BOJ/9020.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int primeNumber[10001];
+
+bool isPrimeNumber(int num)
+{
+    int k = 1, cnt = 1;
+
+    if (primeNumber[num] != -1){
+        return primeNumber[num] == 1;
+    }
+    while (k != num){
+        if (num % k == 0){
+            cnt++;
+        }
+        k++;
+    }
+    if (cnt == 2){
+        primeNumber[num] = 1;
+        return true;
+    }
+    primeNumber[num] = 0;
+    return false;
+}
+
+int main()
+{
+    int T, N;
+
+    cin >> T;
+    memset(primeNumber, -1, sizeof(primeNumber));
+    for (int i = 0; i < T; i++){
+        cin >> N;
+        int num = N / 2;
+        while (num != 1){
+            if (isPrimeNumber(num) && isPrimeNumber(N - num)){
+                cout << num << " " << N - num << "\n";
+                break;
+            }
+            num--;
+        }
+    }
+
+    return 0;
+}

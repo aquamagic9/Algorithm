07e2a8e419abd4f26eb287349e32fdf9a69de610
hyuncho <84334547+aquamagic9@users.noreply.github.com>
2024-05-07 12:29:13 +0900

    1325

--- algorithm/BOJ/1325.cpp
+++ algorithm/BOJ/1325.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <cstring>
+
 using namespace std;
 
 int maxCnt = 0;
-vector<int> result;
 int N, M;
+vector<int> result;
 vector<int> arr[10001];
+int visited[10001];
 
 int dfs(int num){
-    if(arr[num].empty()){
-        return 1;
-    }
-    int cnt = 0;
+    visited[num] = 1;
+    int cnt = 1;
     for (int i = 0; i < arr[num].size(); i++){
+        if (visited[arr[num][i]])
+            continue;
         cnt += dfs(arr[num][i]);
     }
     return cnt;
@@ -32,8 +35,8 @@ int main()
         if (arr[i].empty()){
             continue;
         }
+        memset(visited, 0, sizeof(visited));
         int cnt = dfs(i);
-        cout << i << " " << cnt << "\n";
         if (maxCnt < cnt){
             result.clear();
             result.push_back(i);

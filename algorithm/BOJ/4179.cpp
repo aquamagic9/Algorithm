ac75dd2e38ab7915188be8827b5b268751a5449c
hyuncho <84334547+aquamagic9@users.noreply.github.com>
2024-05-08 21:32:21 +0900

    4179(resolve)

--- algorithm/BOJ/4179.cpp
+++ algorithm/BOJ/4179.cpp
@@ -1,56 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 const int maxInt = 2147483647;
-const int fireAndWall = 0;
-const int playerVisited = 0;
-const int canMove = 1;
 
-int R, C, minValue;
+int R, C, result = 0;
 vector<vector<char>> map;
-vector<vector<int>> visited;
-vector<vector<int>> pVisited;
+vector<vector<int>> personMap;
+vector<vector<int>> fireMap;
 
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
-
-void dfs(const pair<int, int> &pos, const vector<pair<int, int>> &fireZone, int distance)
-{
-    int x = pos.first;
-    int y = pos.second;
-
-    pVisited[y][x] = playerVisited;
-    if (x == 0 || x == C - 1 || y == 0 || y == R - 1){
-        minValue = min(minValue, distance);
-        pVisited[y][x] = canMove;
-        return;
-    }
-
-    vector<pair<int,int>> newFireZone;
-    for (const auto &fire : fireZone){
-        for (int i = 0; i < 4; i++){
-            int fx = fire.first + dx[i];
-            int fy = fire.second + dy[i];
-            if (fx >= 0 && fx < C && fy >= 0 && fy < R && visited[fy][fx] == canMove){
-                newFireZone.push_back(make_pair(fx, fy));
-                visited[fy][fx] = fireAndWall;
-            }
-        }
-    }
-
-    for (int i = 0; i < 4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if (nx >= 0 && nx < C && ny >= 0 && ny < R && pVisited[ny][nx] == canMove && visited[ny][nx] == canMove){
-            dfs(make_pair(nx, ny), newFireZone, distance + 1);
-        }
-    }
-    for (const auto &fire : newFireZone){
-        visited[fire.second][fire.first] = canMove;
-    }
-    pVisited[y][x] = canMove;
-}
 
 int main()
 {
@@ -58,40 +19,70 @@ int main()
 
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    vector<pair<int, int>> fireZone;
-    pair<int, int> jPos;
-    minValue = maxInt;
 
+    pair<int, int> jpos;
+    queue<pair<int, int>> q;
     for (int i = 0; i < R; i++){
         vector<char> row(C);
-        vector<int> vRow(C, canMove);
-        vector<int> pRow(C, canMove);
+        vector<int> pRow(C, 0);
+        vector<int> fRow(C, maxInt);
         for (int j = 0; j < C; j++){
             cin >> row[j];
-            if (row[j] == '#' || row[j] == 'F'){
-                vRow[j] = fireAndWall;
-                pRow[j] = fireAndWall;
-            }
             if (row[j] == 'F'){
-                fireZone.push_back(make_pair(j, i));
+                q.push(make_pair(j, i));
+                fRow[j] = 1;
             }
-            if (row[j] == 'J'){
-                jPos = {j, i};;
-                pRow[j] = playerVisited;
+            else if (row[j] == 'J'){
+                jpos = {j, i};
             }
         }
         map.push_back(row);
-        visited.push_back(vRow);
-        pVisited.push_back(pRow);
+        personMap.push_back(pRow);
+        fireMap.push_back(fRow);
     }
 
-    dfs(jPos, fireZone, 0);
-
-    if (minValue != maxInt){
-        cout << minValue + 1;
+    while(!q.empty())
+    {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for (int i = 0; i < 4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && nx < C && ny >= 0 && ny < R && map[ny][nx] != '#' && fireMap[ny][nx] == maxInt){
+                q.push(make_pair(nx, ny));
+                fireMap[ny][nx] = fireMap[y][x] + 1;
+            }
+        }
     }
-    else {
-        cout << "IMPOSIBLE";
+
+    q.push(jpos);
+    personMap[jpos.second][jpos.first] = 1;
+
+    while(!q.empty())
+    {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        if (x == 0 || x == C - 1 || y == 0 || y == R - 1){
+            result = personMap[y][x];
+            break;
+        }
+        for (int i = 0; i < 4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && nx < C && ny >= 0 && ny < R && personMap[ny][nx] == 0 && map[ny][nx] != '#' && fireMap[ny][nx] > personMap[y][x] + 1){
+                q.push(make_pair(nx, ny));
+                personMap[ny][nx] = personMap[y][x] + 1;
+            }
+        }
+    }
+
+    if (result == 0){
+        cout << "IMPOSSIBLE";
+    }
+    else{
+        cout << result;
     }
 
     return 0;

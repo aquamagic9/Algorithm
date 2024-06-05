0ad2a4d27a4b6599e0734e90e21c17c967268b09
hyuncho <84334547+aquamagic9@users.noreply.github.com>
2024-06-05 12:04:38 +0900

    1799

--- algorithm/BOJ/1799.cpp
+++ algorithm/BOJ/1799.cpp
@@ -1,57 +1,49 @@
 #include <iostream>
 using namespace std;
 
-int mapSize, maxCnt = 0;
+int mapSize;
 int map[10][10];
+int lCross[18], rCross[18];
+int answer[2];
 
-void visitMap(int x, int y)
+void tracking(int row, int col, int count, int color)
 {
-    map[y][x] = 0;
-    int dx[4] = {1,1,-1,-1};
-    int dy[4] = {-1,1,-1, 1};
-    int nx, ny;
-    for (int i = 1; i <= 10; i++){
-        for (int j = 0; j < 4; j++){
-            nx = x + i * dx[j];
-            ny = y + i * dy[j];
-            if (nx >= 0 && ny >= 0 && ny < mapSize && nx < mapSize && map[ny][nx] == 1){
-                map[ny][nx] = 0;
-            }
-        }
+    if (col >= mapSize){
+        row++;
+        if (col % 2 == 0)
+            col = 1;
+        else
+            col = 0;
     }
-}
-
-void backTracking(pair<int, int> pos, int cnt)
-{
-    maxCnt = max(maxCnt, cnt);
-    int newMap[10][10];
-    copy(&map[0][0], &map[0][0] + 100, &newMap[0][0]);
-
-    int x = pos.first;
-    int y = pos.second;
-    for (int i = y; i < mapSize; i++){
-        for (int j = 0; j < mapSize; j++){
-            if (i == y && j < x)
-                continue;
-            if (map[i][j] == 1){
-                visitMap(j, i);
-                backTracking({j, i}, cnt + 1);
-                copy(&newMap[0][0], &newMap[0][0] + 100, &map[0][0]);
-            }
-        }
+    if (row >= mapSize){
+        answer[color] = max(answer[color], count);
+        return;
     }
+    if (map[row][col] && !lCross[mapSize - 1 - row + col] && !rCross[row + col])
+    {
+        lCross[mapSize - 1 - row + col] = 1;
+        rCross[row + col] = 1;
+        tracking(row, col + 2, count + 1, color);
+        lCross[mapSize - 1 - row + col] = 0;
+        rCross[row + col] = 0;
+    }
+    tracking(row, col + 2, count, color);
 }
 
 int main()
 {
+    cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
     cin >> mapSize;
     for (int i = 0; i < mapSize; i++){
         for (int j = 0; j < mapSize; j++){
             cin >> map[i][j];
         }
     }
-    backTracking({0, 0}, 0);
-    cout << maxCnt;
+
+    tracking(0,0,0,0);
+    tracking(0,1,0,1);
+
+    cout << answer[0] + answer[1];
 
     return 0;
 }

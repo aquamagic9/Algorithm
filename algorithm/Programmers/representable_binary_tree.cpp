3532284404a918663cc07ae1a3aaf60da56bf5b0
aquamagic9 <84334547+aquamagic9@users.noreply.github.com>
2025-10-04 22:26:04 +0900

    representable_binary_tree

---
+++ algorithm/Programmers/representable_binary_tree.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+string numToBinaryString(long long numbers){
+    string s1 = "";
+    int k = log(numbers) / log(2) + 1;
+    int n = 1, strLength = 1;
+    while (numbers >= pow(2, strLength)){
+        n *= 2;
+        strLength += n;
+    }
+    for (int i = 0; i < strLength - k; i++)
+        s1 += "0";
+
+    string s2 = "";
+    while (numbers > 0){
+        if (numbers % 2)
+            s2 = "1" + s2;
+        else
+            s2 = "0" + s2;
+        numbers /= 2;
+    }
+    return s1 + s2;
+}
+
+bool checkRepresentableBinaryTree(string s){
+    if (s.size() == 1)
+        return true;
+    if (s[s.size() / 2] == '0'){
+        if (s[s.size() / 4] == '1'|| s[s.size() / 2 + 1 + s.size() / 4] == '1'){
+            return false;
+        }
+    }
+    bool check1 = checkRepresentableBinaryTree(s.substr(0, s.size() / 2));
+    bool check2 = checkRepresentableBinaryTree(s.substr(s.size() / 2 + 1, s.size() / 2));
+    return check1 && check2;
+}
+
+vector<int> solution(vector<long long> numbers) {
+    vector<int> answer;
+
+    for (long long n : numbers) {
+        string s = numToBinaryString(n);
+        if (checkRepresentableBinaryTree(s))
+            answer.push_back(1);
+        else
+            answer.push_back(0);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    //vector<long long> numbers = {7, 42, 5};
+    //vector<long long> numbers = {63, 111, 95, 3712, 16};
+    vector<long long> numbers = {130};
+
+    vector<int> answer = solution(numbers);
+    for (int n : answer){
+        cout << n << " ";
+    }
+
+    return 0;
+}
